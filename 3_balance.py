3a406fa869afdf94d4321b42c1e0df5669c05898
Li <ekinasm@gmail.com>
2016-07-03 07:49:00 +0800

    peculiar balance problem done

--- 3_balance.py
+++ 3_balance.py
@@ -1,39 +1,70 @@
+from math import log,ceil
+
 def answer(x):
 
-    def get_weight(x):
-        if (x <= 4):
+    def get_level(x):
+        num = abs(x)
+        if (num == 1):
+            return 0
+        elif num <= 4:
             return 1
         else:
-            weight = 1
-            while x > (pow(3, weight+1) - 1)/2:
-                weight += 1
-            return weight
+            level = 1
+            while num > (pow(3, level+1) - 1)/2:
+                level += 1
+            return level
 
     # initialize the list
-    to_measure = x
-    weight = get_weight(to_measure)
-    print "the number has a weight of : " + str(weight)
-    result = ['-'] * (weight+1)
-    result[weight] = 'R'
-
-    remain = pow(3, weight) - to_measure
-    while abs(remain) > 1:
-        weight = get_weight(abs(remain))
-        if weight > 0:
-            result[weight-1] = 'L'
+    left = [x]
+    right = []
+    difference = -x
+    while difference != 0:
+        level = get_level(difference)
+        # right is bigger
+        if difference > 0:
+        # left is bigger
+            left.append(pow(3, level))
         else:
-            result[weight-1] = 'R'
-        to_measure = abs(remain)
-        remain = pow(3, weight) - to_measure
+            right.append(pow(3, level))
+        difference = sum(right) - sum(left)
 
-    if remain == 1:
-        result[0] = 'R'
-    else:
-        result[0] = 'L'
+    # turn into weight
+    # print left
+    # print right
 
-    return ''.join(result)
+    result = ['-'] * (get_level(x) + 1)
+    # remove itself
+    left.remove(x)
+    for c in left:
+        result[int(ceil(log(c, 3)))] = 'L'
+    for c in right:
+        result[int(ceil(log(c, 3)))] = 'R'
+    return result
 
 
 if __name__ == "__main__":
-    # print get_weight(3281)
-    print answer(8)
+    # print get_level(3281)
+    # print (answer(122))
+    left = 0
+    right = 0
+    for i in range(1000):
+        result = ''.join(answer(i+1))
+        # print "number : " + str(i) + ", result: " + ''.join(answer(i))
+        # print result
+        len_result = len(result)
+        for j in range(len_result):
+            if result[j] == 'L':
+                left += pow(3, j)
+            elif result[j] == 'R':
+                right += pow(3, j)
+            else:
+                pass
+        if left + i + 1 == right:
+            print "correct! number : " + str(i+1) + ", result: " + result
+        else:
+            print "wrong! number : " + str(i+1) + ", result: " + result
+        left = 0
+        right = 0
+
+    # print "number : " + str(865) + ", result: " + ''.join(answer(865))
+
