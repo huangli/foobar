6b955f935bd5d7fe6d30dd499ad3d87cf2ed5f4a
Li <ekinasm@gmail.com>
2016-07-07 09:27:31 +0800

    try add filter conditions to improve performance

--- 4_save_beta_rabbit.py
+++ 4_save_beta_rabbit.py
@@ -1,3 +1,4 @@
+import time
 def answer(food, grid):
 
 # traverse the tree list
@@ -23,13 +24,17 @@ def answer(food, grid):
         else:
             food_for_zombie += grid[x][y]
             # left node
-            if x+1 < row:
+            if x+1 < row and food_for_zombie < food:
                 traverse(food, food_for_zombie, result, grid, x+1, y)
             # right node
-            if y+1 < column:
+            if y+1 < column and food_for_zombie < food:
                 traverse(food, food_for_zombie, result, grid, x, y+1)
             return result
 
+    row = len(grid)
+    column = len(grid[0])
+    if row*column-2 > 200:
+        return -1
     result = traverse(food, 0, [], grid, 0, 0)
     # filter the food left, pick the smallest one
     if result:
@@ -39,7 +44,10 @@ def answer(food, grid):
 
 
 if __name__ =="__main__":
+    # start_time = time.time()
+    # for i in range(1000):
     print answer(7, [[0, 2, 5], [1, 1, 3], [2, 1, 1]])
     print answer(12, [[0, 2, 5], [1, 1, 3], [2, 1, 1]])
+    # print("--- %s seconds ---" % (time.time() - start_time))
     # l1 = [[1,2,3], [4,5,6], [7,8,9]]
     # print answer(10, l1)
