7cd7c5ba607a90af572f22530a4dce92198a2080
Li <ekinasm@gmail.com>
2016-07-11 15:36:51 +0800

    minlish lession test case 1 passed

--- 5_Minglish_lession.py
+++ 5_Minglish_lession.py
@@ -9,30 +9,41 @@ def answer(words):
             if words[i][j] != words[i+1][j]:
                 sort_list.append([words[i][j],words[i+1][j]])
 
+    # print sort_list
+    temp = sort_list.pop()
     result = []
-    for i in range(words_len-1):
-        # if order exist
-        temp = sort_list[i]
-        # print "Round: " + str(i)
-        # print "result: " + str(result)
-        # print "temp " + ''.join(temp)
-        if temp[0] in result and temp[1] not in result:
-            idx = result.index(temp[0])
-            result = result[:idx] + [temp[1]] + result[idx:]
-        elif temp[1] in result and temp[0] not in result:
-            idx = result.index(temp[1]) - 1
-            result = result[:idx] + [temp[0]] + result[idx:]
-        # not exist, just append
-        elif temp[1] not in result and temp[0] not in result:
-            result.append(temp[0])
-            result.append(temp[1])
-            # insert in the middle
-            # insert at the end
+    result.append(temp[0])
+    result.append(temp[1])
+    remove_list = []
+    while sort_list:
+        # print "round ----:"
+        # print sort_list
+        # print result
+        for temp in sort_list:
+            # raw_input("Press Enter to continue...")
+            # print temp
+            if temp[0] == result[-1]:
+                remove_list.append(temp)
+                result.append(temp[1])
+            elif temp[1] == result[0]:
+                remove_list.append(temp)
+                result.insert(0, temp[0])
+            elif temp[0] in result and temp[1] in result:
+                remove_list.append(temp)
+            else:
+                pass
+        # clear the to be removed list index
+        # print "to be removed list"
+        # print remove_list
+        for temp in remove_list:
+            sort_list.remove(temp)
+        remove_list = []
 
-    return result
+    return ''.join(result)
 
 
 
 if __name__ == "__main__":
-    print answer(["y", "z", "xy"])
-    print answer(["z", "yx", "yz"])
+    # print answer(["y", "z", "xy"])
+    # print answer(["z", "yx", "yz"])
+    print answer(["a", "ba", "bc","c"])
