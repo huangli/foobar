ef201677f445bb35bf2d6fa46a2d397e189ee510
Li <ekinasm@gmail.com>
2016-07-13 11:17:54 +0800

    add minion iterrogation problem, finish minglish lession problem

--- 5_Minglish_lession.py
+++ 5_Minglish_lession.py
@@ -1,49 +1,59 @@
 def answer(words):
     words_len = len(words)
-    sort_list = []
+    sort_order_dict= {}
+
+
+    def find_edge(s1, s2):
+        min_words_len = min(len(s1), len(s2))
+        for i in range(min_words_len):
+            if s1[i] != s2[i]:
+                return s1[i], s2[i]
+
 
     # get sort list
     for i in range(words_len-1):
-        min_words_len = min(len(words[i]), len(words[i+1]))
-        for j in range(min_words_len):
-            if words[i][j] != words[i+1][j]:
-                sort_list.append([words[i][j],words[i+1][j]])
-
-    # print sort_list
-    temp = sort_list.pop()
-    result = []
-    result.append(temp[0])
-    result.append(temp[1])
-    remove_list = []
-    while sort_list:
-        # print "round ----:"
-        # print sort_list
-        # print result
-        for temp in sort_list:
-            # raw_input("Press Enter to continue...")
-            # print temp
-            if temp[0] == result[-1]:
-                remove_list.append(temp)
-                result.append(temp[1])
-            elif temp[1] == result[0]:
-                remove_list.append(temp)
-                result.insert(0, temp[0])
-            elif temp[0] in result and temp[1] in result:
-                remove_list.append(temp)
+        edge = find_edge(words[i], words[i+1])
+        if edge:
+            # print start_node, end_node
+            start_node, end_node = edge
+            if start_node in sort_order_dict:
+                sort_order_dict[start_node].append(end_node)
             else:
-                pass
-        # clear the to be removed list index
-        # print "to be removed list"
-        # print remove_list
-        for temp in remove_list:
-            sort_list.remove(temp)
-        remove_list = []
+                sort_order_dict[start_node] = [end_node]
 
-    return ''.join(result)
 
+    # find the start node
+    end_nodes = set()
+    for nodes in sort_order_dict.values():
+        for node in nodes:
+            end_nodes.add(node)
+
+    start_node = set()
+    for node in sort_order_dict:
+        if node not in end_nodes:
+            start_node.add(node)
+
+    # add first sort pair
+    # print sort_order_dict    temp = start_node.pop()
+    result = []
+    visited_node = []
+    # while sort_order_dict
+        # print "round ----:"
+        # print sort_order_dict        # print result
+    def visit(node):
+        # print 'node: ' + node
+        if node not in visited_node:
+            visited_node.append(node)
+            if node in sort_order_dict:
+                for edge in sort_order_dict[node]:
+                    visit(edge)
+            result.append(node)
+
+    visit(start_node.pop())
+    return ''.join(result[::-1])
 
 
 if __name__ == "__main__":
-    # print answer(["y", "z", "xy"])
-    # print answer(["z", "yx", "yz"])
-    print answer(["a", "ba", "bc","c"])
+    print answer(["y", "z", "xy"])
+    print answer(["z", "yx", "yz"])
+    print answer(["dd", "e", "f", "fa", "fb", "fbb", "fbd"])
